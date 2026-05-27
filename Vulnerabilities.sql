616d69262ef8212370d65fd048aac0c7e0234d40
Moisés de Carvalho <moisesmdecarvalho@gmail.com>
2026-05-27 16:44:04 -0300

    Update Vulnerabilities.sql

--- Vulnerabilities.sql
+++ Vulnerabilities.sql
@@ -1,8 +1,7 @@
 CREATE TABLE IF NOT EXISTS Vulnerabilities (
     Vuln_id INTEGER PRIMARY KEY,
     Asset_id INTEGER,
-    CVE_Identifier VARCHAR(20), -- For strings like 'CVE-2021-44228'
-    Severity VARCHAR(15),       -- For 'Critical', 'High', 'Medium'
-    Is_Patched BOOLEAN,         -- Stores a binary state (True/False or 1/0)
-    FOREIGN KEY (Asset_id) REFERENCES Assets(Asset_id)
+    CVE_Identifier VARCHAR(20),
+    Severity VARCHAR(15),       
+    Is_Patched BOOLEAN,       
 );
